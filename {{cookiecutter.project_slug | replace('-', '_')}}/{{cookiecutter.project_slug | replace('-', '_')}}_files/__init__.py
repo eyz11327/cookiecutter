e792bd2b55da3b673eead1774fea4f7322c707a6
eyz11327 <skyfrank800@gmail.com>
2024-09-15 18:10:55 -0600

    added hyphen replace in init run func

--- {{cookiecutter.project_slug | replace('-', '_')}}/{{cookiecutter.project_slug | replace('-', '_')}}_files/__init__.py
+++ {{cookiecutter.project_slug | replace('-', '_')}}/{{cookiecutter.project_slug | replace('-', '_')}}_files/__init__.py
@@ -1,6 +1,6 @@
 from loguru import logger
 
 def run(secret_config, cwd):
-    logger.info("Hello {{ cookiecutter.project_slug }}!")
+    logger.info("Hello {{ cookiecutter.project_slug | replace('-', '_') }}!")
 
     logger.info("Goodbye.")
